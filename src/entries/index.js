901edd028d008ba80738c7c589d0acbd568d1bbf
xuxiaomeng <xuxiaomeng@baozou.com>
2020-05-12 14:37:07 +0800

    ver

--- src/entries/index.js
+++ src/entries/index.js
@@ -2,10 +2,15 @@ import init from './init';
 
 import './style.scss';
 
-if (__DEV__) {
-  init();
-} else {
-  document.addEventListener('deviceready', () => {
-    init();
-  }, false);
-}
+// if (__DEV__) {
+//   init();
+// } else {
+//   document.addEventListener('deviceready', () => {
+//     init();
+//   }, false);
+// }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('deviceready', init, false);
+}, false);
