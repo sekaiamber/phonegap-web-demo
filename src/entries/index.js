77b2493d1c7bb4a1a3bbea1d6776b6c41dc95c94
xuxiaomeng <xuxiaomeng@baozou.com>
2020-05-12 14:18:58 +0800

    ver.

--- src/entries/index.js
+++ src/entries/index.js
@@ -2,11 +2,10 @@ import init from './init';
 
 import './style.scss';
 
-// if (__DEV__) {
-//   init();
-// }
-
-init();
-
-// document.addEventListener('deviceready', () => {
-// }, false);
+if (__DEV__) {
+  init();
+} else {
+  document.addEventListener('deviceready', () => {
+    init();
+  }, false);
+}
